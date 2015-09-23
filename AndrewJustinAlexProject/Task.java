349dd56e4c3d3fcd398a8ad2a5a76189a787233f
Drewpew <agcummings11@gmail.com>
2015-09-22 18:54:48 -0700

    Update Task.java

--- AndrewJustinAlexProject/Task.java
+++ AndrewJustinAlexProject/Task.java
@@ -5,11 +5,12 @@ public class Task
 	public String name;
 	public String description;
 	public int priority;
+	public int daysUntilDue;
 	
-	public Task (String taskName, String taskDescription, int taskPriority){
+	public Task (String taskName, String taskDescription, int taskPriority, int taskDaysDue){
 		name=taskName;
 		description=taskDescription;
 		priority=taskPriority;
-		
+		daysUntilDue=taskDaysDue;
 	}
 }
