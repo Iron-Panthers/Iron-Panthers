cdc69bdeedd54ac7d3f00ffb14c6b46d6d4d9e7d
Legionof7 <justin.zheng@gmail.com>
2015-09-22 17:25:24 -0700

    Create Task.java
    :)

---
+++ AndrewJustinAlexProject/Task.java
@@ -0,0 +1,15 @@
+import java.lang.String;
+
+public class Task
+{
+	public String name;
+	public String description;
+	public int priority;
+	
+	public Task (String taskName, String taskDescription, int taskPriority){
+		name=taskName;
+		description=taskDescription;
+		priority=taskPriority;
+		
+	}
+}
